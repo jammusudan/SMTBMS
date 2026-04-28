8082e0895c2052d80f56739aea3b0d187d32bab8
ANISH A S <7717823I106@kce.ac.in>
2026-04-28 10:32:26 +0530

    docs: add script to remove vendors

---
+++ backend/scratch/delete_vendors.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Vendor = require('../src/models/Vendor');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const deleteVendors = async () => {
+    try {
+        const uri = process.env.MONGO_URI;
+        let connected = false;
+
+        try {
+            await mongoose.connect(uri, { serverSelectionTimeoutMS: 5000 });
+            connected = true;
+        } catch (error) {
+            const userPass = uri.split('@')[0].replace('mongodb+srv://', '');
+            const fallbackUri = `mongodb://${userPass}@ac-fzgqfl1-shard-00-00.zfjx8br.mongodb.net:27017,ac-fzgqfl1-shard-00-01.zfjx8br.mongodb.net:27017,ac-fzgqfl1-shard-00-02.zfjx8br.mongodb.net:27017/test?ssl=true&replicaSet=atlas-or20t4-shard-0&authSource=admin&retryWrites=true&w=majority`;
+            await mongoose.connect(fallbackUri, { serverSelectionTimeoutMS: 10000 });
+            connected = true;
+        }
+
+        if (connected) {
+            console.log('Deleting all vendors...');
+            const result = await Vendor.deleteMany({});
+            console.log(`Successfully deleted ${result.deletedCount} vendors.`);
+        }
+        process.exit(0);
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+deleteVendors();
